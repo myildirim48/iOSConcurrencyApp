2a20ac3c903fba0ab28cb7b91669ae2c3cb5a1d3
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-06-12 14:12:07 +0300

    Initial Commit

---
+++ iOSConcurrencyApp/iOSConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyAppApp.swift
+//  iOSConcurrencyApp
+//
+//  Created by YILDIRIM on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
